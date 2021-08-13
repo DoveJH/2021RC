8b48002e63b578adc430fc3f490e109e6b172d42
dovejh <1358446393@qq.com>
2021-08-13 15:24:38 +0800

    基本完成yolov5节点

--- src/yolov5/src/yolov5.cpp
+++ src/yolov5/src/yolov5.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include <chrono>
 #include <cmath>
+#include <cv_bridge/cv_bridge.h>
+#include <ros/ros.h>
 #include "cuda_utils.h"
 #include "logging.h"
 #include "common.hpp"
 #include "utils.h"
 #include "calibrator.h"
+#include <opencv2/opencv.hpp>
+#include <sensor_msgs/Image.h>
 
 #define USE_FP32  // set USE_INT8 or USE_FP16 or USE_FP32
 #define DEVICE 0  // GPU id
@@ -21,6 +25,14 @@ static const int OUTPUT_SIZE = Yolo::MAX_OUTPUT_BBOX_COUNT * sizeof(Yolo::Detect
 const char* INPUT_BLOB_NAME = "data";
 const char* OUTPUT_BLOB_NAME = "prob";
 static Logger gLogger;
+ros::Publisher detectingPub;
+ros::Publisher resultPub;
+void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+void* buffers[2];
+static float data[BATCH_SIZE * 3 * INPUT_H * INPUT_W];
+static float prob[BATCH_SIZE * OUTPUT_SIZE];
+cudaStream_t stream;
+IExecutionContext* context;
 
 static int get_width(int x, float gw, int divisor = 8) {
     return int(ceil((x * gw) / divisor)) * divisor;
@@ -297,72 +309,38 @@ bool parse_args(int argc, char** argv, std::string& wts, std::string& engine, bo
     return true;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     cudaSetDevice(DEVICE);
-
-    std::string wts_name = "";
-    std::string engine_name = "";
-    bool is_p6 = false;
-    float gd = 0.0f, gw = 0.0f;
-    std::string img_dir;
-    if (!parse_args(argc, argv, wts_name, engine_name, is_p6, gd, gw, img_dir)) {
-        std::cerr << "arguments not right!" << std::endl;
-        std::cerr << "./yolov5 -s [.wts] [.engine] [s/m/l/x/s6/m6/l6/x6 or c/c6 gd gw]  // serialize model to plan file" << std::endl;
-        std::cerr << "./yolov5 -d [.engine] ../samples  // deserialize plan file and run inference" << std::endl;
-        return -1;
-    }
-
+    ros::init(argc, argv, "yolov5_node");
+    ros::start();
+    ros::NodeHandle n;
     // create a model using the API directly and serialize it to a stream
-    if (!wts_name.empty()) {
-        IHostMemory* modelStream{ nullptr };
-        APIToModel(BATCH_SIZE, &modelStream, is_p6, gd, gw, wts_name);
-        assert(modelStream != nullptr);
-        std::ofstream p(engine_name, std::ios::binary);
-        if (!p) {
-            std::cerr << "could not open plan output file" << std::endl;
-            return -1;
-        }
-        p.write(reinterpret_cast<const char*>(modelStream->data()), modelStream->size());
-        modelStream->destroy();
-        return 0;
-    }
-
-    // deserialize the .engine and run inference
-    std::ifstream file(engine_name, std::ios::binary);
-    if (!file.good()) {
-        std::cerr << "read " << engine_name << " error!" << std::endl;
-        return -1;
-    }
     char *trtModelStream = nullptr;
     size_t size = 0;
-    file.seekg(0, file.end);
-    size = file.tellg();
-    file.seekg(0, file.beg);
-    trtModelStream = new char[size];
-    assert(trtModelStream);
-    file.read(trtModelStream, size);
-    file.close();
-
-    std::vector<std::string> file_names;
-    if (read_files_in_dir(img_dir.c_str(), file_names) < 0) {
-        std::cerr << "read_files_in_dir failed." << std::endl;
-        return -1;
+    std::string engine_name;
+    engine_name ="/home/dovejh/project/RC/RC2021/src/config/engine/yolov5s.engine";
+    //n.getParam("/engine_name", engine_name);
+    std::ifstream file(engine_name, std::ios::binary);
+    if(file.good())
+    {
+        file.seekg(0, file.end);
+        size = file.tellg();
+        file.seekg(0, file.beg);
+        trtModelStream = new char[size];
+        assert(trtModelStream);
+        file.read(trtModelStream, size);
+        file.close();
     }
-
     // prepare input data ---------------------------
-    static float data[BATCH_SIZE * 3 * INPUT_H * INPUT_W];
-    //for (int i = 0; i < 3 * INPUT_H * INPUT_W; i++)
-    //    data[i] = 1.0;
-    static float prob[BATCH_SIZE * OUTPUT_SIZE];
     IRuntime* runtime = createInferRuntime(gLogger);
     assert(runtime != nullptr);
     ICudaEngine* engine = runtime->deserializeCudaEngine(trtModelStream, size);
     assert(engine != nullptr);
-    IExecutionContext* context = engine->createExecutionContext();
+    context = engine->createExecutionContext();
     assert(context != nullptr);
     delete[] trtModelStream;
     assert(engine->getNbBindings() == 2);
-    void* buffers[2];
     // In order to bind the buffers, we need to know the names of the input and output tensors.
     // Note that indices are guaranteed to be less than IEngine::getNbBindings()
     const int inputIndex = engine->getBindingIndex(INPUT_BLOB_NAME);
@@ -375,50 +353,15 @@ int main(int argc, char** argv) {
     // Create stream
     cudaStream_t stream;
     CUDA_CHECK(cudaStreamCreate(&stream));
+    ROS_WARN("engine init done!");
 
-    int fcount = 0;
-    for (int f = 0; f < (int)file_names.size(); f++) {
-        fcount++;
-        if (fcount < BATCH_SIZE && f + 1 != (int)file_names.size()) continue;
-        for (int b = 0; b < fcount; b++) {
-            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
-            if (img.empty()) continue;
-            cv::Mat pr_img = preprocess_img(img, INPUT_W, INPUT_H); // letterbox BGR to RGB
-            int i = 0;
-            for (int row = 0; row < INPUT_H; ++row) {
-                uchar* uc_pixel = pr_img.data + row * pr_img.step;
-                for (int col = 0; col < INPUT_W; ++col) {
-                    data[b * 3 * INPUT_H * INPUT_W + i] = (float)uc_pixel[2] / 255.0;
-                    data[b * 3 * INPUT_H * INPUT_W + i + INPUT_H * INPUT_W] = (float)uc_pixel[1] / 255.0;
-                    data[b * 3 * INPUT_H * INPUT_W + i + 2 * INPUT_H * INPUT_W] = (float)uc_pixel[0] / 255.0;
-                    uc_pixel += 3;
-                    ++i;
-                }
-            }
-        }
-
-        // Run inference
-        auto start = std::chrono::system_clock::now();
-        doInference(*context, stream, buffers, data, prob, BATCH_SIZE);
-        auto end = std::chrono::system_clock::now();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << "ms" << std::endl;
-        std::vector<std::vector<Yolo::Detection>> batch_res(fcount);
-        for (int b = 0; b < fcount; b++) {
-            auto& res = batch_res[b];
-            nms(res, &prob[b * OUTPUT_SIZE], CONF_THRESH, NMS_THRESH);
-        }
-        for (int b = 0; b < fcount; b++) {
-            auto& res = batch_res[b];
-            //std::cout << res.size() << std::endl;
-            cv::Mat img = cv::imread(img_dir + "/" + file_names[f - fcount + 1 + b]);
-            for (size_t j = 0; j < res.size(); j++) {
-                cv::Rect r = get_rect(img, res[j].bbox);
-                cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
-                cv::putText(img, std::to_string((int)res[j].class_id), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
-            }
-            cv::imwrite("_" + file_names[f - fcount + 1 + b], img);
-        }
-        fcount = 0;
+    ros::Subscriber imageSub = n.subscribe("/MVCamera/image_raw", 1, &imageCallback);
+    detectingPub = n.advertise<sensor_msgs::Image>("/detectingResult", 1);
+    ros::Rate loop_rate(100);
+    while(ros::ok())
+    {
+        ros::spinOnce();
+		loop_rate.sleep();
     }
 
     // Release stream and buffers
@@ -429,15 +372,41 @@ int main(int argc, char** argv) {
     context->destroy();
     engine->destroy();
     runtime->destroy();
-
-    // Print histogram of the output distribution
-    //std::cout << "\nOutput:\n\n";
-    //for (unsigned int i = 0; i < OUTPUT_SIZE; i++)
-    //{
-    //    std::cout << prob[i] << ", ";
-    //    if (i % 10 == 0) std::cout << std::endl;
-    //}
-    //std::cout << std::endl;
-
     return 0;
+}
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    cv::Mat img = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8)->image;
+    if (!img.empty())
+    {
+        cv::Mat pr_img = preprocess_img(img,  INPUT_W, INPUT_H);
+        int i = 0;
+        for (int row = 0; row < INPUT_H; ++row) {
+            uchar* uc_pixel = pr_img.data + row * pr_img.step;
+            for (int col = 0; col < INPUT_W; ++col) {
+                data[i] = (float)uc_pixel[2] / 255.0;
+                data[i + INPUT_H * INPUT_W] = (float)uc_pixel[1] / 255.0;
+                data[i + 2 * INPUT_H * INPUT_W] = (float)uc_pixel[0] / 255.0;
+                uc_pixel += 3;
+                ++i;
+            }
+        }
+    }
+
+    // Run inference
+    doInference(*context, stream, buffers, data, prob, BATCH_SIZE);
+    std::vector<Yolo::Detection> batch_res;
+    nms(batch_res, &prob[0], CONF_THRESH, NMS_THRESH);
+    for (int b = 0; b < batch_res.size(); b++) 
+    {
+        auto& res = batch_res[b];
+        for (size_t j = 0; j < batch_res.size(); j++) 
+        {
+            cv::Rect r = get_rect(img, batch_res[j].bbox);
+            cv::rectangle(img, r, cv::Scalar(0x27, 0xC1, 0x36), 2);
+            cv::putText(img, std::to_string((int)batch_res[j].class_id + 1), cv::Point(r.x, r.y - 1), cv::FONT_HERSHEY_PLAIN, 1.2, cv::Scalar(0xFF, 0xFF, 0xFF), 2);
+        }
+    }
+    sensor_msgs::ImagePtr detectingResult = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
+    detectingPub.publish(detectingResult);
 }
